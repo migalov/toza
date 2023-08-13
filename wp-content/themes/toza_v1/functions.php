520528b59d13171e8034c29c9231e001e52e10c1
Michel Muniagiseny <mafri.michel@gmail.com>
2023-08-13 20:00:46 +0300

    TOZA-2 Mobile menu

--- wp-content/themes/toza_v1/functions.php
+++ wp-content/themes/toza_v1/functions.php
@@ -6,6 +6,17 @@
   add_action( 'wp_enqueue_scripts', 'theme_add_scripts' );
   function theme_add_scripts() {
     // подключаем файл стилей темы
-    wp_enqueue_style( 'style-css', TOZA_THEME_ROOT . '/css/style.css' );
+    wp_enqueue_style( 'style-css', TOZA_THEME_ROOT . '/css/style.css', array(), null );
+    wp_enqueue_style( 'popup-css', TOZA_THEME_ROOT . '/css/popup.css', array(), null );
+    wp_enqueue_style( 'header-css', TOZA_THEME_ROOT . '/css/header.css', array(), null );
+    wp_enqueue_style( 'normalize-css', TOZA_THEME_ROOT . '/css/normalize.min.css', array(), null );
   }
+
+  function  theme_register_nav_menu () {
+    register_nav_menus ( array (
+        'top'  =>  'Меню в шапке'
+    ));
+  }
+
+  add_action ( 'after_setup_theme' , 'theme_register_nav_menu' );
 ?>
--- wp-content/themes/toza_v1/index.php
+++ wp-content/themes/toza_v1/index.php
@@ -1,6 +1,41 @@
 <?php get_header() ?>
 <main>
-  COntent
+  <h1>UI Elements</h1>
+  <h2>Icons</h2>
+  <div class="icons">
+    <img class="icon" src="<? echo TOZA_THEME_ROOT ?>/img/icons/Account.svg" alt="">
+  </div>
+  <!--<div class="popup popup--show">-->
+  <div class="popup">
+    <div class="navigation-mobile">
+      <?php 
+        wp_nav_menu(array(
+          'theme_location' => 'top',
+          'container' => null,
+          'container_class' => 'navigation-mobile',
+          'menu_class' => 'navigation-mobile__list',
+          'menu_id' => 'nav'
+        ));
+      ?>
+      <div class="current-user">
+        <img class="current-user__avatar" src="<? echo TOZA_THEME_ROOT ?>/img/avatar_user.png" alt="Джин Дуо">
+        <div class="current-user-info">
+          <span class="current-user-info__title">Джин Дуо</span>
+          <a class="current-user-info__email" href="mailto:jin.duo@gmail.com">jin.duo@gmail.com</a>
+        </div>
+        <ul class="current-user__list">
+          <li class="current-user__list-item">
+            <a class="current-user__item-link" href="/account"><img class="icon" src="<? echo TOZA_THEME_ROOT ?>/img/icons/Account.svg" alt="Личный кабинет">Личный кабинет</a>
+          </li>
+          <li class="current-user__list-item">
+            <a class="current-user__item-link" href="/logout"><img class="icon" src="<? echo TOZA_THEME_ROOT ?>/img/icons/Exit.svg" alt="Выйти">Выйти</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
+  
 </main>
 
 <?php get_footer() ?>
